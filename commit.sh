facd7e2c825a0502161d2b059bb6bfadecf1da9b
blahhaha253 <blahhaha253@gmail.com>
2026-09-04 11:32:54 -0600

    pre-de-obfuscated this and then tried again, was hitting invalid points of java code

--- commit.sh
+++ commit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if [-Z "$1"]; then
-  	echo "Usage: $0 \"commit message\""
-	exit 1;
 
+if [ -z "$1" ]; then
+    echo "Usage: $0 \"commit message\""
+    exit 1
 fi
+
 git add .
 git commit -m "$1"
